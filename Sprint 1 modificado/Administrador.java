2906f1b134c749bc4d5271c5e3f50dc392c9abc4
Mateo Wagner <133800290+mateowagner@users.noreply.github.com>
2023-06-30 18:01:15 -0300

    Update Administrador.java

--- Sprint 1 modificado/Administrador.java
+++ Sprint 1 modificado/Administrador.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 public class Administrador extends Usuario{
-    private static ArrayList<Usuario> usuarios;
-    private static ArrayList<Empresa> empresas;
-    private static ArrayList<EmpresaAdherida> empresaAdheridas;
+    //private static ArrayList<Usuario> usuarios;
+    //private static ArrayList<Empresa> empresas;
+    //private static ArrayList<EmpresaAdherida> empresaAdheridas;
+    // las de arriba no van acá, existen en plataforma
     private static String clave = "12345678Ab";
 
     public Administrador(String nombre, String apellido, int dni) {
