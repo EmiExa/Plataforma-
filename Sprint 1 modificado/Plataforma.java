c4fbe9b13c7dce8670fabd1939803e6ee3abdb2f
Facundoacu <133800090+Facundoacu@users.noreply.github.com>
2023-06-30 17:55:44 -0300

    Update Plataforma.java
    Falta generarCompra debatir

--- Sprint 1 modificado/Plataforma.java
+++ Sprint 1 modificado/Plataforma.java
@@ -1,3 +1,6 @@
+mport java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,59 +10,62 @@ public class Plataforma {
     private ArrayList<Pasajero> pasajeros;
     private Banco bancoAsociado;
     private ArrayList<EmpresaAdherida> empresasConvenio;
-    public Plataforma(Banco banco){
+
+    public Plataforma(Banco banco) {
         empresas = new ArrayList<>();
         admins = new ArrayList<>();
         pasajeros = new ArrayList<>();
         empresasConvenio = new ArrayList<>();
         bancoAsociado = banco;
     }
-    public void setBanco(Banco banco){
+
+    public void setBanco(Banco banco) {
         bancoAsociado = banco;
     }
-    public Pasajero login(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Ingresar dni: "); int dni = s.nextInt();
-        s.nextLine(); //descarta el salto de linea genereado por nextint
-        System.out.println("Ingresar clave: "); String clave = s.nextLine();
-        for(int i=0; i<pasajeros.size();i++){
+
+    public Pasajero login(int dni, String clave) {
+        for (int i = 0; i < pasajeros.size(); i++) {
             Pasajero p = pasajeros.get(i);
-            if ((p.getDni() == dni) && (p.getClave().equals(clave))){
-                System.out.println("Has ingresado a la plataforma 9 3/4");
+            if ((p.getDni() == dni) && (p.getClave() == clave)) {
                 return p;
             }
-            System.out.println("Tu dni o contraseña son incorrectos");
         }
         return null;
     }
-    public void registro(){
+
+    public void registro() {
         Scanner s = new Scanner(System.in);
-        System.out.println("Ingresar nombre: "); String nombre = s.nextLine();
-        System.out.println("Ingresar apellido: "); String apellido = s.nextLine();
-        System.out.println("Ingresar dni: "); int dni = s.nextInt();
-        s.nextLine(); //descarta el salto de linea genereado por nextint
-        System.out.println("Ingresar Email: "); String email = s.nextLine();
-        System.out.println("Ingresar clave: "); String clave = s.nextLine();
-        Pasajero p = new Pasajero(nombre,apellido,dni,email,clave);
+        System.out.println("Ingresar nombre: ");
+        String nombre = s.nextLine();
+        System.out.println("Ingresar apellido: ");
+        String apellido = s.nextLine();
+        System.out.println("Ingresar dni: ");
+        int dni = s.nextInt();
+        System.out.println(" ");
+        System.out.println("Ingresar Email: ");
+        String email = s.nextLine();
+        System.out.println("Ingresar clave: ");
+        String clave = s.nextLine();
+        Pasajero p = new Pasajero(nombre, apellido, dni, email, clave);
         System.out.println("Quiere asociar una tarjeta de credito?");
         System.out.println("1) Si" + "\n2) No");
         int aux = s.nextInt();
-        while(aux == 1){
-            System.out.println("Ingresar numero de Tarjeta:"); int num = s.nextInt();
-            s.nextLine(); //descarta el salto de linea genereado por nextint
-            System.out.println("Ingresar marca: "); String marc = s.nextLine();
-            Tarjeta t = bancoAsociado.buscarTarjeta(num,marc);
-
-            if (t != null){
+        while (aux == 1) {
+            System.out.println("Ingresar numero de Tarjeta:");
+            int num = s.nextInt();
+            System.out.println(" ");
+            System.out.println("Ingresar marca: ");
+            String marc = s.nextLine();
+            Tarjeta t = bancoAsociado.buscarTarjeta(num, marc);
+            if (t != null) {
                 p.agregarTarjeta(t);
                 aux = 10;
-            } else
-                System.out.println("No se ha encontrado la tarjeta");
+            }
         }
-        System.out.println("Te has registrado con exito!!");
         this.addPasajero(p);
     }
-   public ArrayList<Viaje> buscarPasaje(LocalDate dia) {
+
+    public ArrayList<Viaje> buscarPasaje(LocalDate dia) {
         Scanner s = new Scanner(System.in);
         System.out.println("Ingresar origen: ");
         String origen = s.nextLine();
@@ -69,7 +75,7 @@ public class Plataforma {
         //convert String to LocalDate
         String dateString= s.nextLine();
         DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("d/MM/yyyy");
-        
+
         LocalDate localDate = LocalDate.parse(dateString, formatter);
 
         ArrayList<Viaje> salida= new ArrayList<>();
@@ -127,5 +133,18 @@ public class Plataforma {
     }
     public void notificarPasajero(Pasajero p, Suscripcion s){
         System.out.println("Te avisamos "+p.getNombre()+ " que el viaje que va de "+s.getOrigen()+" a "+s.getDestino()+" Esta con un descuento por viaje improvisado");
+    }
+    public void generarCompra(Viaje viajes, Pasajero pasajero1){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingresar origen: ");
+        int cantidad = s.nextInt();
+        // solicita cantidad de pasajeros
+        // por cada pasajero pide asiento y datos: dni - funcion aparte
+        // notificar pasajeros
+        // control de cantidad de pasajeros
+        // control de  pago: preguntar si existe tarjeta cargada en el sistema
+        // ignoramos pago con creditos
+        // decrementamos el cantAsientosDisponibles del viaje
+        // 
     }
 }
