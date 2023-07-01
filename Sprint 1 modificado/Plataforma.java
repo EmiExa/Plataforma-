650931d006b3efe4dade491903f9319ca779b915
VictoriaDibar <133808746+VictoriaDibar@users.noreply.github.com>
2023-07-01 19:05:58 -0300

    modificacion de registro y login

--- Sprint 1 modificado/Plataforma.java
+++ Sprint 1 modificado/Plataforma.java
@@ -7,15 +7,13 @@ import java.util.Scanner;
 
 public class Plataforma {
     private ArrayList<Empresa> empresas;
-    private ArrayList<Administrador> admins;
-    private ArrayList<Pasajero> pasajeros;
+    private ArrayList<Usuario> usuarios;
     private Banco bancoAsociado;
     private ArrayList<EmpresaAdherida> empresasConvenio;
 
     public Plataforma(Banco banco) {
         empresas = new ArrayList<>();
-        admins = new ArrayList<>();
-        pasajeros = new ArrayList<>();
+        usuarios = new ArrayList<>();
         empresasConvenio = new ArrayList<>();
         bancoAsociado = banco;
     }
@@ -24,15 +22,22 @@ public class Plataforma {
         bancoAsociado = banco;
     }
 
-    public Pasajero login(int dni, String clave) {
-        for (int i = 0; i < pasajeros.size(); i++) {
-            Pasajero p = pasajeros.get(i);
-            if ((p.getDni() == dni) && (p.getClave() == clave)) {
-                return p;
+    public Pasajero login(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingresar dni: "); int dni = s.nextInt();
+        s.nextLine(); //descarta el salto de linea genereado por nextint
+        System.out.println("Ingresar clave: "); String clave = s.nextLine();
+        for(int i=0; i<usuarios.size();i++){
+            Usuario u = usuarios.get(i);
+            if ((u.getDni() == dni) && (u.getClave().equals(clave))){
+                System.out.println("Has ingresado a la plataforma 9 3/4");
+                return u;
             }
+            System.out.println("Tu dni o contraseña son incorrectos");
         }
         return null;
     }
+    
     public Pasajero buscaPasajero( int dni){
         for (int i = 0; i < pasajeros.size(); i++) {
             Pasajero p = pasajeros.get(i);
@@ -42,38 +47,38 @@ public class Plataforma {
         }
         return null;
     }
-    public void registro() {
+    
+   public void registro(){
         Scanner s = new Scanner(System.in);
-        System.out.println("Ingresar nombre: ");
-        String nombre = s.nextLine();
-        System.out.println("Ingresar apellido: ");
-        String apellido = s.nextLine();
-        System.out.println("Ingresar dni: ");
-        int dni = s.nextInt();
-        System.out.println(" ");
-        System.out.println("Ingresar Email: ");
-        String email = s.nextLine();
-        System.out.println("Ingresar clave: ");
-        String clave = s.nextLine();
-        Pasajero p = new Pasajero(nombre, apellido, dni, email, clave);
-        System.out.println("Quiere asociar una tarjeta de credito?");
-        System.out.println("1) Si" + "\n2) No");
-        int aux = s.nextInt();
-        while (aux == 1) {
-            System.out.println("Ingresar numero de Tarjeta:");
-            int num = s.nextInt();
-            System.out.println(" ");
-            System.out.println("Ingresar marca: ");
-            String marc = s.nextLine();
-            Tarjeta t = bancoAsociado.buscarTarjeta(num, marc);
-            if (t != null) {
-                p.agregarTarjeta(t);
-                aux = 10;
-            }
+        System.out.println("Ingresar nombre: "); String nombre = s.nextLine();
+        System.out.println("Ingresar apellido: "); String apellido = s.nextLine();
+        System.out.println("Ingresar dni: "); int dni = s.nextInt();
+        s.nextLine(); //descarta el salto de linea genereado por nextint
+        System.out.println("Ingresar Email: "); String email = s.nextLine();
+        System.out.println("Ingresar clave: "); String clave = s.nextLine();
+        if (buscaPasejero(dni) == null){
+            Pasajero p = new Pasajero(nombre,apellido,dni,email,clave);
+            System.out.println("Quiere asociar una tarjeta de credito?");
+            System.out.println("1) Si" + "\n2) No");
+            int aux = s.nextInt();
+            if (aux == 1)
+                agregarTarjeta();
+            System.out.println("Te has registrado con exito!!");
+            this.addUsuario(p);
         }
-        this.addPasajero(p);
+   }
+    public vois agregarTarjeta(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingresar numero de Tarjeta:"); int num = s.nextInt();
+        s.nextLine(); //descarta el salto de linea genereado por nextint
+        System.out.println("Ingresar marca: "); String marc = s.nextLine();
+        Tarjeta t = bancoAsociado.buscarTarjeta(num,marc);
+        if (t != null){
+            p.agregarTarjeta(t);
+        } else
+            System.out.println("No se ha encontrado la tarjeta");
     }
-
+    
     public ArrayList<Viaje> buscarPasaje() {
         Scanner s = new Scanner(System.in);
         System.out.println("Ingresar origen: ");
@@ -106,13 +111,15 @@ public class Plataforma {
 
 
     public void addUsuario(Usuario u){
-        pasajeros.add(u);
+        usuario.add(u);
     }
     public void addEmpresa(Empresa emp){
-        empresas.add(emp);
+        if (!empresas.contains(emp))        
+            empresas.add(emp);
     }
     public void addEmpresaConv(EmpresaAdherida emp){
-        empresasConvenio.add(emp);
+        if (!empresasConvenio.contains(emp))
+            empresasConvenio.add(emp);
     }
     public void agregarConvenio(Empresa emp, Double descuento, int horaMin, int asientosMin ){
         EmpresaAdherida empresa = new EmpresaAdherida(emp.getNombre(), descuento, horaMin, asientosMin);
@@ -129,11 +136,11 @@ public class Plataforma {
                     suscripciones.add(s.get(j));
             }
         }
-        for(int i = 0; i< pasajeros.size(); i++){
+        for(int i = 0; i< usuarios.size(); i++){
             for(int j = 0; j<suscripciones.size(); j++) {
                 Suscripcion suscripcion = suscripciones.get(j);
-                if (pasajeros.get(i).suscrito(suscripcion))
-                    this.notificarPasajero(pasajeros.get(i), suscripcion);
+                if (usuarios.get(i).suscrito(suscripcion))
+                    this.notificarPasajero(usuarios.get(i), suscripcion);
             }
         }
     }
