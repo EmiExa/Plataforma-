4e24c17ceffea30b9fdea51dabb01e6975f1274b
Facundoacu <133800090+Facundoacu@users.noreply.github.com>
2023-07-01 18:16:43 -0300

    Update Plataforma.java
    Generar Compra

--- Sprint 1 modificado/Plataforma.java
+++ Sprint 1 modificado/Plataforma.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -6,10 +7,10 @@ import java.util.Scanner;
 
 public class Plataforma {
     private ArrayList<Empresa> empresas;
-    private ArrayList<Usuario> usuarios;
+    private ArrayList<Administrador> admins;
+    private ArrayList<Pasajero> pasajeros;
     private Banco bancoAsociado;
     private ArrayList<EmpresaAdherida> empresasConvenio;
-    private ArrayList<Suscripcion> suscripciones; 
 
     public Plataforma(Banco banco) {
         empresas = new ArrayList<>();
@@ -23,16 +24,24 @@ public class Plataforma {
         bancoAsociado = banco;
     }
 
-    public Usuario login(int dni, String clave) {
-        for (int i = 0; i < usuarios.size(); i++) {
-            Usuario u = usuarios.get(i);
-            if ((u.getDni() == dni) && (u.getClave() == clave)) {
-                return u;
+    public Pasajero login(int dni, String clave) {
+        for (int i = 0; i < pasajeros.size(); i++) {
+            Pasajero p = pasajeros.get(i);
+            if ((p.getDni() == dni) && (p.getClave() == clave)) {
+                return p;
             }
         }
         return null;
     }
-
+    public Pasajero buscaPasajero( int dni){
+        for (int i = 0; i < pasajeros.size(); i++) {
+            Pasajero p = pasajeros.get(i);
+            if (p.getDni() == dni){
+                return p;
+            }
+        }
+        return null;
+    }
     public void registro() {
         Scanner s = new Scanner(System.in);
         System.out.println("Ingresar nombre: ");
@@ -65,7 +74,7 @@ public class Plataforma {
         this.addPasajero(p);
     }
 
-    public ArrayList<Viaje> buscarPasaje(LocalDate dia) {
+    public ArrayList<Viaje> buscarPasaje() {
         Scanner s = new Scanner(System.in);
         System.out.println("Ingresar origen: ");
         String origen = s.nextLine();
@@ -80,7 +89,7 @@ public class Plataforma {
 
         ArrayList<Viaje> salida= new ArrayList<>();
         for (int i = 0; i < empresas.size(); i++) {
-            ArrayList<Viaje> viajes = empresas.get(i).buscarPasajes(dia,origen,destino);
+            ArrayList<Viaje> viajes = empresas.get(i).buscarPasajes(localDate,origen,destino);
             if (viajes != null){
                 salida.addAll(viajes);
             }
@@ -97,10 +106,10 @@ public class Plataforma {
 
 
     public void addPasajero(Pasajero p){
-        usuarios.add(p);
+        pasajeros.add(p);
     }
     public void addAdmin(Administrador a){
-        usuarios.add(a);
+        admins.add(a);
     }
     public void addEmpresa(Empresa emp){
         empresas.add(emp);
@@ -126,55 +135,11 @@ public class Plataforma {
         for(int i = 0; i< pasajeros.size(); i++){
             for(int j = 0; j<suscripciones.size(); j++) {
                 Suscripcion suscripcion = suscripciones.get(j);
-                if (usuarios.get(i).suscrito(suscripcion))
-                    this.notificarPasajero(usuarios.get(i), suscripcion);
+                if (pasajeros.get(i).suscrito(suscripcion))
+                    this.notificarPasajero(pasajeros.get(i), suscripcion);
             }
         }
     }
-
-    public void suscribirseViaje(Pasajero p){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Ingresar el origen al que se quiere suscribir:");
-        String origen = s.nextLine();
-        System.out.println("Ingresar el destino al que se quiere suscribir:");
-        String destino = s.nextLine();   
-        int i = 0;
-        boolean encontrado = false;
-        while ((i<suscripciones.size())&&(encontrado = false)){
-            Suscripcion suscActual = suscripciones.get(i);
-            if ((origen.equals(suscActual.getOrigen()))&&(destino.equals(suscActual.getDestino()))){
-                //Si existe una suscripcion de viaje con ese origen y destino se agrega el pasajero a esta.
-                suscActual.addPasajero(p);
-                encontrado = true;
-            }
-            i++;
-        }
-        if ((encontrado = false)&&(origen != destino)){
-            Suscripcion sus = new Suscripcion(origen,destino);
-            sus.addPasajero(p);
-            suscripciones.add(sus);
-        }
-        
-    }
-    
-    public void darseBaja(Pasajero p){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Ingresar el origen de la suscripcion para darse de baja:");
-        String origen = s.nextLine();
-        System.out.println("Ingresar el destino de la suscripcion para darse de baja:");
-        String destino = s.nextLine();  
-        int i = 0;
-        boolean encontrado = false;
-        while ((i<suscripciones.size())&&(encontrado = false)){
-            Suscripcion suscActual = suscripciones.get(i);
-            if ((origen.equals(suscActual.getOrigen()))&&(destino.equals(suscActual.getDestino()))){
-                suscActual.eliminarPasajero(p);
-                encontrado = true;
-            }
-            i++;
-        }
-    }
-    
     public void notificarPasajero(Pasajero p, Suscripcion s){
         System.out.println("Te avisamos "+p.getNombre()+ " que el viaje que va de "+s.getOrigen()+" a "+s.getDestino()+" Esta con un descuento por viaje improvisado");
     }
@@ -182,13 +147,25 @@ public class Plataforma {
         Scanner s = new Scanner(System.in);
         System.out.println("Ingresar origen: ");
         int cantidad = s.nextInt();
-        // solicita cantidad de pasajeros
-        // por cada pasajero pide asiento y datos: dni - funcion aparte
-        // notificar pasajeros
-        // control de cantidad de pasajeros
-        // control de  pago: preguntar si existe tarjeta cargada en el sistema
+        if (viajes.getCantAsientosDisponibles() >= cantidad){
+            for (int i = 0; i < cantidad; i++) {
+                System.out.println("Ingrese su dni: " +"\n");
+                int dni = s.nextInt();
+                Pasajero p = this.buscaPasajero(dni);
+                if (p == null){
+                    registro();
+                }
+            }
+            viajes.setCantAsientosDisponibles(cantidad);
+        }
+        if(pasajero1.getTarjeta() != null) {
+
+            bancoAsociado.cobrar(pasajero1.getTarjeta(),viajes.getMonto()*cantidad);
+            //Enviar mail de notificiacion
+        }
+        else {
+            //Solicitar datos de tarjeta
+        }
         // ignoramos pago con creditos
-        // decrementamos el cantAsientosDisponibles del viaje
-        // 
     }
 }
