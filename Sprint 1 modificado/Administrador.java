51694c628eeea45c42d5c6b444f64e4f5fc4591d
VictoriaDibar <133808746+VictoriaDibar@users.noreply.github.com>
2023-07-01 18:39:26 -0300

    Update Administrador

--- Sprint 1 modificado/Administrador.java
+++ Sprint 1 modificado/Administrador.java
@@ -1,38 +1,26 @@
 import java.util.ArrayList;
 
 public class Administrador extends Usuario{
-    //private static ArrayList<Usuario> usuarios;
-    //private static ArrayList<Empresa> empresas;
-    //private static ArrayList<EmpresaAdherida> empresaAdheridas;
-    // las de arriba no van acá, existen en plataforma
+    private static int colesMin = 5;
     private static String clave = "12345678Ab";
 
     public Administrador(String nombre, String apellido, int dni) {
         super(nombre,apellido,dni);
-        this.usuarios = new ArrayList<>();
-        this.empresas = new ArrayList<>();
-        this.empresaAdheridas = new ArrayList<>();
-    }
-
-    public void getUsuarios(){
-        ArrayList<Usuario> usuario = new ArrayList<>();
-        usuario.addAll(usuarios);
-        return usuario;
     }
   
-        public String getClave() {
+    public String getClave() {
         return this.clave();
     }
-  
 
-    public void login(String nombre, int dni, String clave){
-        //if((super.getNombre().equals(nombre)) && (this.clave.equals(clave)) && (super.getDni() ==dni)) {
-            //loguear
-        }
-        //else{}
-        //nologuear
+    public void addConvenio(Empresa convenio, Plataforma plataforma){
+        int cantColes = convenio.getCantColectivos();
+        if (cantColes > colesMin)
+            plataforma.addEmpresa(convenio);
+    }
 
-
-
+    public void verEstadisticas(){
+        //Queda para implementar
+    }
+    
 
 }
