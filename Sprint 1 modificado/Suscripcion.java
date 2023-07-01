cd5a63589b9c654c1111b179d7637db793b10d41
Mateo Wagner <133800290+mateowagner@users.noreply.github.com>
2023-07-01 17:36:34 -0300

    Update Suscripcion.java
    agregue lista de pasajeros como atributo y el metodo addpasajero

--- Sprint 1 modificado/Suscripcion.java
+++ Sprint 1 modificado/Suscripcion.java
@@ -1,9 +1,14 @@
 public class Suscripcion {
     private String origen;
     private String destino;
+    private Arraylist<Pasajero> pasajeros;
     public Suscripcion(String origen, String destino){
         this.origen = origen;
         this.destino = destino;
+        this.pasajeros = new ArrayList<>();
+    }
+    public void addPasajero(Pasajero p){
+            pasajeros.add(p);
     }
     public boolean equals(Object s){
         if (s instanceof Suscripcion) {
