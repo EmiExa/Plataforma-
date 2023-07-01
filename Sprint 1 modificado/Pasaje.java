f8cb7b826c2a142cd463f9f457135c81a8c765cb
Mateo Wagner <133800290+mateowagner@users.noreply.github.com>
2023-07-01 18:20:08 -0300

    Update Pasaje.java
    Adición de atributos a la clase Pasaje

--- Sprint 1 modificado/Pasaje.java
+++ Sprint 1 modificado/Pasaje.java
@@ -1,10 +1,13 @@
 public class Pasaje {
 
     private Pasajero pasajero;
+    private int asiento;
     private Double valor;
     private Viaje viaje;
     
-    public Pasaje(Pasajero pasajero, Viaje viaje, Double costo){
+    public Pasaje(Pasajero pasajero, Viaje viaje, int asiento, Double costo) {
+        this.pasajero = pasajero;
+        this.asiento = asiento;
         this.viaje = viaje;
         valor = costo;
     }
