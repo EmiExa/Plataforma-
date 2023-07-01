0fb71250b034bccd4611b9edc535df7f8b732456
Mateo Wagner <133800290+mateowagner@users.noreply.github.com>
2023-07-01 18:17:49 -0300

    Update Pasaje.java

--- Sprint 1 modificado/Pasaje.java
+++ Sprint 1 modificado/Pasaje.java
@@ -1,10 +1,12 @@
 public class Pasaje {
 
-    //revisar si hay que agregar pasajero, sino sacarlo del diagrama
+    private Pasajero pasajero;
     private Double valor;
     private Viaje viaje;
-    public Pasaje(Viaje viaje, Double costo){
+    
+    public Pasaje(Pasajero pasajero, Viaje viaje, Double costo){
         this.viaje = viaje;
         valor = costo;
     }
+    
 }
