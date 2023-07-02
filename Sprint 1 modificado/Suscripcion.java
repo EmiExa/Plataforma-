be3ba67e4947193a8aeaf69ac13f3adec0e9e738
Facundoacu <133800090+Facundoacu@users.noreply.github.com>
2023-07-02 18:46:30 -0300

    Update Suscripcion.java

--- Sprint 1 modificado/Suscripcion.java
+++ Sprint 1 modificado/Suscripcion.java
@@ -1,17 +1,17 @@
-public class Suscripcion {
+{
     private String origen;
     private String destino;
-    private Arraylist<Pasajero> pasajeros;
+    private ArrayList<Pasajero> pasajeros;
     public Suscripcion(String origen, String destino){
         this.origen = origen;
         this.destino = destino;
         this.pasajeros = new ArrayList<>();
     }
     public void addPasajero(Pasajero p){
-            pasajeros.add(p);
+        pasajeros.add(p);
     }
     public void eliminarPasajero(Pasajero p){
-        pasajero.remove(p);
+        pasajeros.remove(p);
     }
     public boolean equals(Object s){
         if (s instanceof Suscripcion) {
@@ -26,5 +26,17 @@ public class Suscripcion {
     }
     public String getDestino(){
         return destino;
+    }
+    public void notificar(Viaje v){
+        for(Pasajero p:pasajeros){
+            //para cada pasajero se le envia un mail con el viaje en descuento
+        }
+    }
+    public boolean existePasajero(Pasajero p){
+        for(Pasajero pasajero:pasajeros){
+            if(p.equals(pasajero))
+                return true;
+        }
+        return false;
     }
 }
