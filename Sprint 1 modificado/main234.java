27c747ed666e1de19c4c4d0d0a555a62e15e1d21
Mateo Wagner <133800290+mateowagner@users.noreply.github.com>
2023-07-02 20:12:35 -0300

    Update main234.java

--- Sprint 1 modificado/main234.java
+++ Sprint 1 modificado/main234.java
@@ -1,40 +1,197 @@
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Scanner;
 
-public class Banco {
-    private ArrayList<Tarjeta> tarjetas;
-    private String nombre;
-    public Banco(String nombre){
-        this.nombre = nombre;
-        tarjetas = new ArrayList<>();
+public class main234 {
+    public main234() {
     }
-    public void addTarjeta(Tarjeta tar){
-        tarjetas.add(tar);
+
+    public void cargarEmpresasEstatica(Plataforma p) {
+        //Evita el menu de administrador para cargar las empresas mas rapido y poder probarlo.
+        Administrador admin = new Administrador("Pia","Benediti",12345);
+        //CREACION EMPRESAS
+        Empresa e1 = new Empresa("El gorrion");
+        Empresa e2 = new Empresa("Langueyu");
+        Empresa e3 = new Empresa("Condor Estrella");
+        EmpresaAdherida e4 = new EmpresaAdherida("Plusmar",0.5,2,5);
+        EmpresaAdherida e5 = new EmpresaAdherida("Flechabus",0.4,3,5);
+        EmpresaAdherida e6 = new EmpresaAdherida("El Rapido",0.8,3,9);
+
+        //CREACION COLECTIVOS
+        Colectivo c1 = new Colectivo(1, 30, e1);
+        Colectivo c2 = new Colectivo(1, 20, e4);
+
+        //CREACION VIAJES
+        Viaje v1 = new Viaje("Tandil", "MarDelPlata",  c1, LocalDateTime.now(), 1500.0);
+        Viaje v2 = new Viaje("Tandil", "Azul",c2, LocalDateTime.now(), 2000.0);
+        Viaje v3 = new Viaje("Tandil", "Ayacucho",  c1, LocalDateTime.now(), 5000.0);
+        Viaje v4 = new Viaje("Tandil", "Olavarria",  c2, LocalDateTime.now(), 1200.0);
+        Viaje v5 = new Viaje("Olavarria", "Tandil", c1, LocalDateTime.now(), 6000.0);
+
+        //CARGA DE PASAJERO DEFAULT
+        Pasajero lu = new Pasajero("Lu","Delgado",1,"","1");
+        Pasajero vico = new Pasajero("Vico","Dibar",44870875,"","1");
+        Pasajero del = new Pasajero("Delfi","Ferreri",44377571,"","1");
+        
+        //AGREGO COLECTIVOS Y VIAJES A LAS EMPRESAS
+        e1.addColectivo(c1);
+        e1.addViaje(v1);
+        e1.addViaje(v3);
+
+        e4.addColectivo(c2);
+        e4.addViaje(v2);
+        e4.addViaje(v4);
+        e4.addViaje(v5);
+        
+        //SE AGREGAN PASAJEROS
+        p.addUsuario(lu);
+        p.addUsuario(vico);
+        p.addUsuario(del);
+
+        //SE AGREGAN LAS EMPRESAS A LA PLATAFORMA
+        admin.addConvenio(e1, p);
+        admin.addConvenio(e2, p);
+        admin.addConvenio(e3, p);
+        admin.addConvenioAdherido(e4, p);
+        admin.addConvenioAdherido(e5, p);
+        admin.addConvenioAdherido(e6, p);
     }
-    
-    public boolean cobrar(Tarjeta tar, double monto){
-        for (int i = 0; i < tarjetas.size(); i++) {
-            Tarjeta aux = tarjetas.get(i);
-            if (aux.getNum() == tar.getNum()){
-                // if cobro es exitoso
-                return true;
+
+    public void mostrarMenuPlataforma (Pasajero p, Plataforma nueve){
+        int opcion = 0;
+        while(opcion != 6){
+            System.out.println("MENU: ");
+            System.out.println(" 1.Buscar pasajes" + "\n 2.Rastreo" + "\n 3.Suscribirse a viaje improvisado" +"\n 4.Cancelar compra" + "\n 5.Baja de viaje improvisado" +"\n 6.Salir");
+            Scanner s = new Scanner(System.in);
+            System.out.println("Ingrese el numero de opcion que desea realizar");
+            opcion = s.nextInt();
+            s.nextLine();
+            switch(opcion){
+                case 1:
+                    //Buscar pasajes
+                    ArrayList<Viaje> resultadoBusqueda = nueve.buscarPasaje();
+                    //Elegir asiento
+                    System.out.println("Desea comprar un pasaje? 1.SI 2.NO");
+                    int realizaCompra = s.nextInt();
+                    if (realizaCompra == 1) {
+                        System.out.println("Seleccione el numero de viaje que desea comprar:");
+                        int op = s.nextInt();
+                        s.nextLine();
+                        if ((op > -1) && (op < resultadoBusqueda.size())){
+                            nueve.generarCompra(resultadoBusqueda.get(op),p);
+                        } else System.out.println("OPCION INCORRECTA");
+                    } else System.out.println("OPCION INCORRECTA");
+                    break;
+                case 2:
+                    //Suscribirse a rastreo
+                    break;
+                case 3:
+                    //Suscribirse a viaje improvisado
+                    nueve.suscribirseViaje(p);
+                    break;
+                case 4:
+                    //Cancelar compra
+                    break;
+                case 5:
+                    //Darse de baja de viaje improvisado
+                    nueve.darseBaja(p);
+                    break;
             }
         }
-        return false;
     }
-    public Tarjeta  buscarTarjeta(int num, String marca){
-        for(int i=0;i<tarjetas.size();i++){
-            Tarjeta t = tarjetas.get(i);
-            if((t.getNum() == num) && (t.getMarca().equals( marca) ))
-                return t;
+
+    public void mostrarMenuPasajero(Plataforma nueve){
+        int opcion = 0;
+        while (opcion != 3){
+            System.out.println("INICIO: ");
+            System.out.println(" 1.Loguearse" + "\n 2.Registrarse" + "\n 3.Salir");
+            System.out.println("Ingrese la opcion que desea realizar:");
+            Scanner s = new Scanner(System.in);
+            opcion = s.nextInt();
+            s.nextLine();
+            if (opcion == 1){
+
+                 Usuario p = nueve.login();
+                if (p != null)
+                    mostrarMenuPlataforma(((Pasajero)p),nueve); //Esto esta bien hay que hacer un casting pero estaria y ver lo del static
+                else{
+                    System.out.println("Su usuario no fue encontrado");
+                }
+            } else if (opcion == 2){
+                nueve.registro();
+            } else if (opcion != 3){
+                System.out.println("La opcion ingresada no es correcta, vuelva a intentar");
+            }
         }
-        return null;
     }
-	public boolean validar(int nroTarjeta) {
-		for(Tarjeta t: tarjetas) {
-			if (t.getNum()==nroTarjeta)
-				return true;
-		}
-		return false;
-			
-	}
+
+    public void mostrarMenuAdmin (Plataforma plat){
+        Scanner s = new Scanner(System.in);
+        boolean ingreso = false;
+        int opcion =0;
+        while(!ingreso){
+            System.out.println("LOGIN");
+            Usuario admin = plat.login();
+            if (admin != null){
+                ingreso = true;
+                while (opcion != 3)
+                System.out.println("MENU ADMINISTRADOR: ");
+                System.out.println("1.Agregar convenio"+ "\n 2.Ver estadisticas" +"\n 3.Salir");
+                System.out.println("Ingrese el numero de opcion que desea realizar:");
+                opcion = s.nextInt();
+                s.nextLine();
+                if (opcion == 1){
+                    //Agregar convenio
+                    String nombre = s.nextLine();
+
+                    int opc = 0;
+                    while ((opc != 1) && (opc != 2)){
+                        System.out.println("Desea que su empresa esta adherida? 1.SI 2.NO");
+                        opc = s.nextInt();
+                        s.nextLine();
+                        if (opc == 1) {
+                            EmpresaAdherida emp = new EmpresaAdherida(nombre, 0.7, 4, 6);
+                            ((Administrador) admin).addConvenioAdherido(emp, plat);
+                        }
+                        else if (opc == 2) {
+                            Empresa empa = new Empresa(nombre);
+                            ((Administrador) admin).addConvenio(empa, plat);
+                        }
+                        if ((opc != 1) && (opc != 2))
+                            System.out.println("La opcion ingresada no es correcta vuelva a intentar");
+                    }
+                }
+                if (opcion == 2){
+                    //Ver Estadisticas
+                    //Queda a implementar
+                }
+                if (opcion != 3){
+                    System.out.println("La opcion ingresada es incorrecta vuelva a intentar");
+                }
+            } else
+                System.out.println("El usuario ingresado es incorrecto vuelva a intentar");
+        }
+    }
+
+    public void menuSeleccion(Plataforma p){
+        int opcion = 0;
+
+        while ((opcion != 1) && (opcion!= 2)){
+            System.out.println("Ingrese 1 si sos usuario o ingresa 2 si sos administrador:");
+            Scanner s = new Scanner(System.in);
+            opcion = s.nextInt();
+            s.nextLine();
+            if (opcion == 1)
+                mostrarMenuPasajero(p); //Falta el usuario/pasajero ver
+            else if (opcion == 2)
+                mostrarMenuAdmin(p);
+            if ((opcion != 1) && (opcion != 2))
+                System.out.println("La opcion ingresada no es correcta vuelva a intentar");
+        }
+
+    }
+
+
 }
