1e5854e4e36e60fce084d06801af902122053f14
Facundoacu <133800090+Facundoacu@users.noreply.github.com>
2023-07-02 18:48:04 -0300

    Update Pasajero.java

--- Sprint 1 modificado/Pasajero.java
+++ Sprint 1 modificado/Pasajero.java
@@ -11,7 +11,7 @@ public class Pasajero extends Usuario{
     public Pasajero(String nombre, String apellido, int dni,Tarjeta tarjeta, String email, String clave){
         this(nombre,apellido,dni,email,clave);
         this.tarjeta = tarjeta;
-        }
+    }
     public Pasajero(String nombre, String apellido, int dni, String email, String clave){
         super(nombre,apellido,dni);
         this.email = email;
@@ -23,20 +23,20 @@ public class Pasajero extends Usuario{
     public void addPasajes(ArrayList<Pasaje> pasajes) {
         historialPasajes.addAll(pasajes);
     }
-    
-    public void addTarjeta() {
-        System.out.print(" Ingresar banco: "); String banco = s.nextString();
-        s.nextLine();
+
+    public void addTarjeta(Banco banco) {
+        Scanner s = new Scanner(System.in);
         System.out.print(" Ingresar número de tarjeta: "); int nroTarjeta = s.nextInt();
         s.nextLine();
-        Tarjeta tarjeta = new Tarjeta(nroTarjeta,banco,"VISA");
+        String marca = "visa";
+        Tarjeta tarjeta = new Tarjeta(nroTarjeta,banco, marca);
         this.tarjeta = tarjeta;
     }
-    
+
     private void cambiarClave(String contraseña){
         //generar la clave con sus condiciones
     }
-    
+
     public boolean suscrito(Suscripcion s){
         return suscripciones.contains(s);
     }
@@ -50,15 +50,6 @@ public class Pasajero extends Usuario{
         }
         return suma;
     }
-
-    //public Arraylist<Pasaje> comprarPasaje(Viaje Viaje, int numeroPasajes){
-        //consulta si ese viaje tiene los asientos disponibles etc...
-        //realiza la transaccion autocompletado etc...
-        //genera los pasajes
-        //agrega el viaje a historial
-        //se fija si en el historial se repite el viaje mas de 3 veces
-        //Si no tiene una suscripcion a ese viaje(origen destino) se le notifica que se puede suscribir al viaje improvisado
-    //}
     public String getClave(){
         return clave;
     }
@@ -66,5 +57,9 @@ public class Pasajero extends Usuario{
         if((super.getDni() == p.getDni()) && (this.getClave() == p.getClave()))
             return true;
         return false;
+    }
+
+    public Tarjeta getTarjeta() {
+        return tarjeta;
     }
 }
